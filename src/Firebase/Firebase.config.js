b3d4ae10c7f1a1b50e4332b3d61a97b99df8bb59
MunnaAhmed130 <munnaahmed2025@gmail.com>
2021-10-29 02:03:00 +0600

    first commit

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAb2z1DZZP5Aw-6HoisG73kcYErcY42iEs",
+    authDomain: "travel-agency-e968e.firebaseapp.com",
+    projectId: "travel-agency-e968e",
+    storageBucket: "travel-agency-e968e.appspot.com",
+    messagingSenderId: "573680585676",
+    appId: "1:573680585676:web:541a7ba67f6771c863ab69"
+};
+
+
+export default firebaseConfig;
