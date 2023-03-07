3c705bad9a06d58af70116968304670f984d8b90
Munna Ahmed <munnaahmed2025@gmail.com>
2023-03-07 14:38:43 +0600

    commit

--- src/Components/TourPlans/TourPlans.js
+++ src/Components/TourPlans/TourPlans.js
@@ -15,23 +15,33 @@ const TourPlans = () => {
     }, []);
 
     return (
-        <section className="tours-section py-5">
-            <h2 className=" fw-bold  text-center">
-                TRAVEL TO YOUR DESTINATION
-            </h2>
+        <>
+            <section className="tours-section">
+                <h2 className=" fw-bold  text-center">
+                    TRAVEL TO YOUR DESTINATION
+                </h2>
 
-            {plans.length === 0 ? (
-                <Spinner animation="border" variant="primary" />
-            ) : (
-                <Container fluid="md">
-                    <Row xs={1} sm={1} md={2} lg={3} className="g-4 m-4">
-                        {plans.map((plan) => (
-                            <Service key={plan._id} service={plan}></Service>
-                        ))}
-                    </Row>
-                </Container>
-            )}
-        </section>
+                {plans.length === 0 ? (
+                    <Spinner animation="border" variant="primary" />
+                ) : (
+                    <Container fluid="md">
+                        <Row xs={1} sm={1} md={2} lg={3} className="g-4 m-4">
+                            {plans.map((plan) => (
+                                <Service
+                                    key={plan._id}
+                                    service={plan}
+                                ></Service>
+                            ))}
+                        </Row>
+                    </Container>
+                )}
+            </section>
+            {/* <img
+                className="tours-btm-img"
+                src="https://i.ibb.co/NFDM48Y/h5-bckg-img-02.jpg"
+                alt=""
+            /> */}
+        </>
     );
 };
 
