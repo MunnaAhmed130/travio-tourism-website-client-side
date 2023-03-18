649fcacb7b2df2df2dc9aecf3e65d07481ec3185
Munna Ahmed <munnaahmed2025@gmail.com>
2023-03-18 20:15:47 +0600

    commit

--- src/Components/Shared/Rating/Rating.js
+++ src/Components/Shared/Rating/Rating.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BsStarFill, BsStar, BsStarHalf } from "react-icons/bs";
-// import { FaStarHalf } from "react-icons/fa";
+import { BsStarFill, BsStar } from "react-icons/bs";
 import "./Rating.css";
 
 const Rating = ({ count, emptyColor, className }) => {
@@ -9,23 +8,7 @@ const Rating = ({ count, emptyColor, className }) => {
         setRating(rating);
     }, []);
 
-    // const empty = (
-    //     (1 - (rating - Math.floor(rating)).toFixed(1)) *
-    //     100
-    // ).toString();
-
     const full = ((rating - Math.floor(rating)).toFixed(1) * 100).toString();
-    // console.log(full);
-    // const broken = {
-    //     position: "absolute",
-    //     backgroundColor: `${emptyColor || "white"} `,
-    //     width: `${empty}%`,
-    //     height: "100%",
-    //     top: "0",
-    //     right: "0",
-    //     bottom: "0",
-    //     zIndex: "8",
-    // };
 
     const hidden = {
         width: `${full}%`,
@@ -82,15 +65,3 @@ const Rating = ({ count, emptyColor, className }) => {
 };
 
 export default Rating;
-
-// {<span className="parent--star">
-// <BsStarHalf className="half-star">
-//     <BsStarFill className="child--star"></BsStarFill>
-//     <span style={broken}></span>
-//     <BsStar className="outline" />
-// </BsStarHalf>
-// {/* <BsStarFill className="child--star"></BsStarFill> */}
-// {/* <span style={broken}></span>
-// <BsStar className="outline" /> */}
-// {/* <BsStarFill className="hidden--star" /> */}
-// </span>}
