0da0059bfd7607895d678203fd5549a00dfb2585
Munna Ahmed <munnaahmed2025@gmail.com>
2023-03-23 21:26:44 +0600

    commit

--- src/Components/Home/TourPlans/TourPlans.js
+++ src/Components/Home/TourPlans/TourPlans.js
@@ -12,8 +12,10 @@ const TourPlans = () => {
 
     useEffect(() => {
         // fetch("fakedata.json")
-        fetch(`http://localhost:4000/tours/query?limit=${limit}`)
-            // fetch(`http://localhost:4000/tours`)
+        fetch(
+            `https://travel-related-website-server.vercel.app/tours/query?limit=${limit}`
+        )
+            // fetch(`http://localhost:4000/tours/query?limit=${limit}`)
             .then((res) => res.json())
             .then((data) => setPlans(data));
     }, []);
@@ -28,7 +30,7 @@ const TourPlans = () => {
             </h2>
             {plans.length === 0 ? (
                 <Container fluid="lg">
-                    <Row xs={1} sm={2} md={2} lg={3} className="g-4 m-1">
+                    <Row xs={1} sm={1} md={2} lg={3} className="g-4 m-1">
                         {limitArray.map((n) => (
                             <SkeletonProduct key={n}></SkeletonProduct>
                         ))}
@@ -36,7 +38,7 @@ const TourPlans = () => {
                 </Container>
             ) : (
                 <Container fluid="lg" className="px-3">
-                    <Row xs={1} sm={2} md={2} lg={3} className="g-4 my-1">
+                    <Row xs={1} sm={1} md={2} lg={3} className="g-4 my-1 ">
                         {plans.map((plan) => (
                             <Service key={plan._id} service={plan}></Service>
                         ))}
