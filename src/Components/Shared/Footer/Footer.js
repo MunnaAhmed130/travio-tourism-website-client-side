44a7a564a84b8ba1d5d26aba73df10577fb8984f
Munna Ahmed <munnaahmed2025@gmail.com>
2023-03-25 04:45:26 +0600

    commit

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -11,14 +11,22 @@ import {
 } from "react-icons/fa";
 import "./Footer.css";
 
-const layout = "py-1 border";
+const layout = "py-4 ";
+const contactList = "lh-1 d-flex flex-row mb-3";
+const contactInfo = "d-flex flex-column justify-content-around";
 const Footer = () => {
     return (
-        <div className="footer">
+        <footer className="footer">
             <Container>
                 <Row className="py-2">
-                    <Col xs={12} sm={12} md={6} xl={3} className={`${layout}`}>
-                        <div className="web-info--container d-flex flex-row justify-content-start align-items-center ">
+                    <Col
+                        xs={12}
+                        sm={12}
+                        md={6}
+                        xl={3}
+                        className={`d-flex flex-column ${layout}`}
+                    >
+                        <div className=" d-flex flex-row justify-content-start align-items-center my-2">
                             <span className="logo-container d-flex position-relative">
                                 <img
                                     className="logo"
@@ -31,10 +39,10 @@ const Footer = () => {
                                 Travio
                             </h3>
                         </div>
-                        <p className="footer-info py-2 mb-0">
+                        <p className="footer-info pt-2 pb-3 mb-0">
                             Our expert Travel Crafters design unique package
-                            trips to exciting destinations, with all the details
-                            covered to save you time and money.
+                            trips to exciting destinations to save you time and
+                            money.
                         </p>
                         <h6 className="text-uppercase">Follow us on</h6>
 
@@ -58,43 +66,60 @@ const Footer = () => {
                         sm={12}
                         md={6}
                         xl={3}
-                        className={`${layout} pages`}
+                        className={`d-flex flex-column ${layout} pages`}
                     >
-                        <h3 className="text-uppercase">Pages</h3>
-                        <Link>Home</Link>
-                        <Link>About Us</Link>
-                        <Link>Privacy Policy</Link>
-                        <Link>Contact Us</Link>
-                        <Link>Terms & conditions</Link>
+                        <h3 className="text-uppercase py-2">Pages</h3>
+                        <nav className="">
+                            <Link to="/">Home</Link>
+                            <Link>About Us</Link>
+                            <Link>Contact Us</Link>
+                            <Link>Privacy Policy</Link>
+                            <Link>Terms & conditions</Link>
+                        </nav>
                     </Col>
-                    <Col xs={12} sm={12} md={6} xl={3} className={`${layout}`}>
-                        <h3 className="text-uppercase">Contact Us</h3>
-                        <div className="contact-info">
-                            <div className="lh-1">
-                                <span className="icon-container">
-                                    <BsTelephoneFill />
-                                </span>
-                                <p className="d-inline-block">
+                    <Col
+                        xs={12}
+                        sm={12}
+                        md={6}
+                        xl={3}
+                        className={`d-flex flex-column contact-us  ${layout}`}
+                    >
+                        <h3 className="text-uppercase py-2">Contact Us</h3>
+                        {/* <div className="contact-info"> */}
+                        <div className={`${contactList}`}>
+                            <span className="icon-container">
+                                <BsTelephoneFill />
+                            </span>
+                            <div className={`${contactInfo} contact-info`}>
+                                <span className="">Drop a Line</span>
+                                <p className="d-inline-block m-0">
                                     +00 (123) 456 890
                                 </p>
                             </div>
-                            <div className="lh-1">
-                                <span className="icon-container">
-                                    <MdEmail />
-                                </span>
-                                <p className="d-inline-block">
+                        </div>
+                        <div className={`${contactList}`}>
+                            <span className="icon-container">
+                                <MdEmail />
+                            </span>
+                            <div className={`${contactInfo} contact-info`}>
+                                <span>Email Address</span>
+                                <p className="d-inline-block m-0">
                                     travio@email.com
                                 </p>
                             </div>
-                            <div className="lh-1">
-                                <span className="icon-container">
-                                    <MdLocationOn />
-                                </span>
-                                <p className="d-inline-block">
+                        </div>
+                        <div className={`${contactList}`}>
+                            <span className="icon-container">
+                                <MdLocationOn />
+                            </span>
+                            <div className={`${contactInfo} contact-info`}>
+                                <span>Visit Office</span>
+                                <p className="d-inline-block m-0">
                                     583 Main Street, NY, USA
                                 </p>
                             </div>
                         </div>
+                        {/* </div> */}
                     </Col>
                     <Col xs={12} sm={12} md={6} xl={3} className={`${layout}`}>
                         <h3 className="text-uppercase">instagram Gallary</h3>
@@ -106,7 +131,7 @@ const Footer = () => {
                     All Right Reserved &copy;2023 Copyrights by TRAVIO
                 </p>
             </div>
-        </div>
+        </footer>
     );
 };
 
